798935a047bfa212e171fa0cab49dac0d1c954e2
Drew Myers <drew.myers59@gmail.com>
2022-09-07 21:37:12 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using StaticKeywordExercise;
+using System;
+
+namespace StaticKeyWordExercise
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(212);
+            Console.WriteLine($"Celsius: {celsius}");
+            Console.WriteLine("");
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(0);
+            Console.WriteLine($"Fahrenheit: {fahrenheit}"); 
+        }
+    }
+}
